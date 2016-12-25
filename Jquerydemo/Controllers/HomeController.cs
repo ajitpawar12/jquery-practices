00f5693aed6ef11b3eb731893c691ae5ac6b3504
ajit <ajit@idpsolutions.com.au>
2016-12-24 16:40:20 -0800

    jq-01 first commit

--- Jquerydemo/Controllers/HomeController.cs
+++ Jquerydemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jquerydemo.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Jquerydemo.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : JQBaseController
     {
         public ActionResult Index()
         {
